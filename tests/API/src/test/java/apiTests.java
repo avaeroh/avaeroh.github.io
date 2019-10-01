b2897e802d8dfa919a41cf78ff99bbb01be790f6
Jonathan Millington-Hotze <Jonathan.Millington-Hotze@centre4.local>
2019-10-01 14:25:26 +0100

    adding API testing

---
+++ tests/API/src/test/java/apiTests.java
@@ -0,0 +1,22 @@
+import io.restassured.http.Header;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class apiTests {
+
+    private static final String baseUrl = "https://pokeapi.co/api/v2/";
+    private static final String cyndaquilResourceID = "pokemon/cyndaquil/";
+    private static final int cydaquilsWeight = 79;
+    private Header acceptHeader = new Header("accept", "text/json");
+
+    @Test
+    public void test() {
+        Response response = given().header(acceptHeader).get(baseUrl + cyndaquilResourceID);
+        int actualWeight = Integer.parseInt(response.then().extract().path("weight").toString());
+        assertEquals(actualWeight, cydaquilsWeight);
+    }
+
+}
